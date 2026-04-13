5d0bb516c1b2193d22dcbf8cfa51f6e91d0c14be
zzhuravleff <aqua197604@gmail.com>
2026-04-13 18:49:40 +0300

    fix btnAdd

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -332,7 +332,7 @@ export default function SubjectPage() {
             )}
 
             <Button 
-                className="mt-4" 
+                className="w-full" 
                 size="lg" 
                 isDisabled={
                     typeTask === "Расписание" 
